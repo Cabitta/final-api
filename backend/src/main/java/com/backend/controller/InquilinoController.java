87866bc11c64d01da7fd97cf1b5581c314d1ceda
franco <fr.cabanillas7@gmail.com>
2024-12-10 20:38:01 -0300

    inquilino

--- backend/src/main/java/com/backend/controller/InquilinoController.java
+++ backend/src/main/java/com/backend/controller/InquilinoController.java
@@ -4,6 +4,8 @@ import com.backend.entity.InquilinoEntity;
 import com.backend.repository.InquilinoRepository;
 import com.backend.service.InquilinoService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +16,11 @@ import java.util.List;
 @AllArgsConstructor
 public class InquilinoController {
     private InquilinoService service;
-    private InquilinoRepository repo;
+    @Autowired
+    private InquilinoRepository repository;
 
+    @GetMapping
     public List<InquilinoEntity> getAll() {
-        return repo.findAll();
+        return repository.findAll();
     }
 }
--- backend/src/main/java/com/backend/repository/InquilinoRepository.java
+++ backend/src/main/java/com/backend/repository/InquilinoRepository.java
@@ -2,10 +2,11 @@ package com.backend.repository;
 
 import com.backend.entity.InquilinoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface InquilinoRepository extends JpaRepository<InquilinoEntity, Long> {
 
-    List<InquilinoEntity> findAllInquilinos();
+    List<InquilinoEntity> findAll();
 }
